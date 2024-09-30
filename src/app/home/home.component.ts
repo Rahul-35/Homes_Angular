7ba60868daa20f416bce3211da4340a2c2f84efd
Rahul Ch <xavi3441@gmail.com>
2024-10-01 00:10:26 +0530

    2nd version- added services for DI

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housing-location';
+import {CommonModule} from '@angular/common';
+import { HousingService } from '../housing.service';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  //readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocationList: HousingLocation[] = [];
+
+  housingService:HousingService=inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
